b535baf691d0abd362bb598fde1023bfcd167c87
rohitgr7 <rohitgr1998@gmail.com>
2020-08-12 03:27:59 +0530

    Fix code and add solution

--- Task1/ex2.py
+++ Task1/ex2.py
@@ -1,3 +1,4 @@
-torch_std = torch.arange(10000,dtype=torch.float32).std(unbiased=False).numpy()
-numpy_std = np.arange(10000).std()
+x = torch.arange(10000).float()
+numpy_std = x.numpy().std()
+torch_std = x.std(unbiased=False)
 assert np.allclose(torch_std,numpy_std),"Not the same!"
